0ff2a749c72f6fd3d4acf2045a72ea78d6b42aa7
Weston Gibler <weston@bluekeel.com>
2019-08-20 19:40:25 -0600

    adding graphql to handler

--- api/handler.js
+++ api/handler.js
@@ -1,11 +1,33 @@
 'use strict';
 
+const server = require("apollo-server-lambda");
+const schema = require('./schema');
+
+exports.graphql = server.graphqlLambda((event, context) => {
+  const headers = event.headers;
+  const functionName = context.functionName;
+
+   return {
+       schema: schema,
+       context: {
+           headers,
+           functionName,
+           event,
+           context
+       }
+   };
+});
+
+exports.graphiql = server.graphiqlLambda({
+    endpointURL: '/Prod/graphql'
+});
+
 module.exports.hello = async event => {
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
+        message: 'Who farted?',
         input: event,
       },
       null,
